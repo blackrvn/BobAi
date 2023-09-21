e34618e16464e3c050c0c70eec76f714b0582f4f
blackrvn <71042074+blackrvn@users.noreply.github.com>
2023-09-21 08:16:19 +0200

    Refactored vectorstore

--- main.py
+++ main.py
@@ -1,88 +1,44 @@
-import spacy.cli.download
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import PyPDFDirectoryLoader
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import LlamaCpp
-from langchain.prompts import PromptTemplate
-from langchain.text_splitter import SpacyTextSplitter
-from langchain.vectorstores import Chroma
+from Bob import Bob
+import pandas as pd
+from tqdm import tqdm
+from icecream import ic
 
-TOK_MODEL = "de_core_news_sm"
-PATH_PDF = "./pdf"
-PATH_DB = "./db"
-PATH_MODEL = "./Models/openbuddy-llama2-70b-v10.1-q3_k.gguf"
-
-# Load PDF
-
-loader = PyPDFDirectoryLoader(PATH_PDF)
-documents = loader.load()
-
-# Tokenization
-try:
-    tokenizer = SpacyTextSplitter(
-        pipeline=TOK_MODEL,
-        chunk_size=500,
-        chunk_overlap=50,
-        separator='\n'
-    )
-except OSError:
-    spacy.cli.download(TOK_MODEL)
-
-split_doc = tokenizer.split_documents(documents)
-
-
-# Embedding
-embedding = HuggingFaceEmbeddings(
-    model_name="intfloat/multilingual-e5-small",
-    model_kwargs={
-        'device': 'cuda'
-    },
-    encode_kwargs={
-        'normalize_embeddings': True
-    }
-)
-
-# Vectorstore
-store = Chroma.from_documents(
-    collection_name='BobStore',
-    documents=split_doc,
-    embedding=embedding,
-    persist_directory=PATH_DB
-)
-
-# Retriever
-retriever = store.as_retriever(search_kwargs={"k": 5})
-
-# Chain
-
-template = """Use the following pieces of context to answer the question at the end. 
-If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-Use three sentences maximum and keep the answer as concise as possible. 
+bob = Bob(promt_template="""Use the following pieces of context to answer the question at the end. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer.  
 Answer in German.
 {context}
 Question: {question}
-Helpful Answer:"""
+Helpful Answer:""")
 
-chain_prompt = PromptTemplate.from_template(template)
+# bob.add_docs()
 
-llm = LlamaCpp(
-    model_path=PATH_MODEL,
-    max_tokens=2048,
-    verbose=True,
-    n_ctx=2048,
-    n_gpu_layers=22,
-    temperature=0.1
-)
+answers = {
+    "Questions": [],
+    "Temperatures": [],
+    "Answers": [],
+    "Sources": [],
+    "Raw Result": []
+}
 
-retrieval_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    retriever=retriever,
-    return_source_documents=True,
-    chain_type_kwargs={
-        'prompt': chain_prompt
-    }
-)
 
-question = "Auf welcher Höhe sollte sich ein Handlauf befinden?"
-result = retrieval_chain({'query': question})
-print(result["result"])
+def ask(temp, q):
+    answers["Questions"].append(q)
+    answers["Temperatures"].append(temp)
+    result = bob.init_chain(temp, q)
+    answers["Answers"].append(result["result"])
+    answers["Sources"].append(result["source_documents"])
+    answers["Raw Result"].append(result)
+
+
+# Testing
+with open("perfomance_questions.txt") as file_questions:
+    questions = file_questions.readlines()
+    for question in tqdm(questions, total=len(questions)):
+        ic(f"Current question: {question}")
+        ask(0, question)
+        ask(0.1, question)
+        ask(0.25, question)
+        ask(0.5, question)
+
+    df = pd.DataFrame(answers)
+    df.to_csv("C:/Users/vruser01/OneDrive - P.ARC AG/100_Arbeit/perfomance_answers.csv")
